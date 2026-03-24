aae1890acbe46fd640fe71411cae7af676059807
MAKOMIKOLAKO <reachmaako@gmail.com>
2026-03-24 13:33:53 -0400

    ibvgf

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -3,5 +3,7 @@ import { authOptions } from "@/lib/authOptions";
 
 export async function getCurrentUserId() {
   const session = await getServerSession(authOptions);
+  console.log("Session:", session);
+  console.log("User ID:", (session?.user as any)?.id);
   return (session?.user as any)?.id ?? null;
 }
