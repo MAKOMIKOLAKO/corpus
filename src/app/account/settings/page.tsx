e0d76281a263e42aed7c9073e769f99d3e8e4c1e
MAKOMIKOLAKO <reachmaako@gmail.com>
2026-03-24 17:40:21 -0400

    fix: move 'use client' directive to top of file
    - Fix webpack build error by placing 'use client' before all imports
    - This resolves the build failure and allows deployment

--- src/app/account/settings/page.tsx
+++ src/app/account/settings/page.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 'use client';
 
+import React from 'react';
 import { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter, useSearchParams } from 'next/navigation';
