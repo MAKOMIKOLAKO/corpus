16392d4cf0c54c2a8fb44b1285771eb12dc83811
MAKOMIKOLAKO <reachmaako@gmail.com>
2026-03-26 12:50:53 -0400

    Fix feedback popup overlay - add semi-transparent backdrop
    - Changed DialogPrimitive.Overlay background from transparent to bg-black/50
    - This prevents underlying content from showing through the popup
    - Makes the feedback modal consistent with the add collection modal style

--- src/components/FeedbackModal.tsx
+++ src/components/FeedbackModal.tsx
@@ -26,7 +26,7 @@ const CustomDialogContent = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
 >(({ className, children, ...props }, ref) => (
   <DialogPrimitive.Portal>
-    <DialogPrimitive.Overlay className="fixed inset-0 z-[9998] bg-transparent data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
+    <DialogPrimitive.Overlay className="fixed inset-0 z-[9998] bg-black/50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
